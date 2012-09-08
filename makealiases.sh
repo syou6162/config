f8c59fa821f6f93a1d06e178d9e8c91633d84482
Yasuhisa Yoshida <syou6162@gmail.com>
2012-09-08 17:54:11 +0900

    シンボリックリンクを貼るスクリプトを追加

---
+++ makealiases.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# $HOME以下に設定ファイル群へのシンボリックリンクを貼る
+for filename in emacs.d emacs zshrc oh-my-zsh screenrc Rprofile gitconfig gitignore
+do
+    if [ ! -e $HOME/.$filename ]; then
+	ln -s `pwd`/.$filename $HOME/.$filename
+    fi
+done
+
+# oh-my-zsh用にシンボリックリンクを貼る
+for file in custom/*
+do
+    if [ ! -e $HOME/.oh-my-zsh/$file ]; then
+	ln -s `pwd`/$file $HOME/.oh-my-zsh/$file
+    fi
+done
