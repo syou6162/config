18d580f60f91d5a56eff6b5f30305d62d436e835
Yasuhisa Yoshida <syou6162@gmail.com>
2015-05-27 12:02:31 +0900

    cliでエラーになるsettingを削除

--- .emacs.d/inits/00-init.el
+++ .emacs.d/inits/00-init.el
@@ -32,64 +32,6 @@
 (setenv "PATH"
   (concat "/opt/local/bin:" (getenv "PATH")))
 
-(when (>= emacs-major-version 23)
- (setq fixed-width-use-QuickDraw-for-ascii t)
- (setq mac-allow-anti-aliasing t)
- (set-face-attribute 'default nil
-                     :family "monaco"
-                     :height 140)
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0208
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0212
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'katakana-jisx0201
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
- ;;; Unicode フォント
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'mule-unicode-0100-24ff
-  '("monaco" . "iso10646-1"))
-;;; キリル，ギリシア文字設定
-;;; 注意： この設定だけでは古代ギリシア文字、コプト文字は表示できない
-;;; http://socrates.berkeley.edu/~pinax/greekkeys/NAUdownload.html が必要
-;;; キリル文字
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'cyrillic-iso8859-5
-  '("monaco" . "iso10646-1"))
-;;; ギリシア文字
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'greek-iso8859-7
-  '("monaco" . "iso10646-1"))
- (setq face-font-rescale-alist
-       '(("^-apple-hiragino.*" . 1.2)
-         (".*osaka-bold.*" . 1.2)
-         (".*osaka-medium.*" . 1.2)
-         (".*courier-bold-.*-mac-roman" . 1.0)
-         (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-         (".*monaco-bold-.*-mac-roman" . 0.9)
-         ("-cdac$" . 1.3))))
-
-(setq default-frame-alist
-      (append (list '(foreground-color . "azure3")
-		    '(background-color . "black")
-		    '(border-color . "black")
-		    '(mouse-color . "white")
-		    '(cursor-color . "white")
-		    '(width . 100)
-		    '(height . 30)
-		    '(top . 0)
-		    '(left . 10)
-		    '(alpha . (80 60)))    ;;ここ
-		    default-frame-alist))
-
 (tool-bar-mode -1)
 
 ;;;emacsのkillingの履歴
