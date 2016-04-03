3e1ea065ce4c391e566d484c932a67e59fca629d
Yasuhisa Yoshida <syou6162@gmail.com>
2016-04-03 14:44:33 +0900

    EDITOR変数を変更

--- custom/osx.zsh
+++ custom/osx.zsh
@@ -1,7 +1,6 @@
 case "${OSTYPE}" in
 darwin*)
 	alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
-	alias emacsclient='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
-	export EDITOR='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
+	export EDITOR=emacsclient
 	;;
 esac
