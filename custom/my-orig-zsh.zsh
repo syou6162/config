733d6a1fc1f71886f1f58676ce7ec5d6fbeb96ff
Yasuhisa Yoshida <syou6162@gmail.com>
2016-04-14 10:18:49 +0900

    ndenvの追加

--- custom/my-orig-zsh.zsh
+++ custom/my-orig-zsh.zsh
@@ -120,5 +120,8 @@ if which plenv > /dev/null; then eval "$(plenv init -)"; fi
 export PATH="$HOME/.rbenv/bin:$PATH"
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
+export PATH="$HOME/.ndenv/bin:$PATH"
+if which ndenv > /dev/null; then eval "$(ndenv init -)"; fi
+
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
