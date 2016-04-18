bb2a5bb77d443355ba6b336b478934b301f148c7
Yasuhisa Yoshida <syou6162@gmail.com>
2016-04-19 08:23:49 +0900

    コマンド履歴もpecoで検索できるように

--- custom/my-orig-zsh.zsh
+++ custom/my-orig-zsh.zsh
@@ -156,3 +156,19 @@ function peco-insert-selected-git-files(){
 
 zle -N peco-insert-selected-git-files
 bindkey "^x^s" peco-insert-selected-git-files
+
+function peco-select-history() {
+  local tac
+  if which tac > /dev/null; then
+    tac="tac"
+  else
+    tac="tail -r"
+  fi
+  BUFFER=$(\history -n 1 | \
+	      eval $tac | \
+	      peco --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
