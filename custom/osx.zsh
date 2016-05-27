219bdc66838f88d42543ad8de4af8705a4d5d35d
Yasuhisa Yoshida <syou6162@gmail.com>
2016-05-27 20:34:09 +0900

    jupyter notebookのエイリアスを作った

--- custom/osx.zsh
+++ custom/osx.zsh
@@ -2,5 +2,6 @@ case "${OSTYPE}" in
 darwin*)
 	alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
 	export EDITOR=emacsclient
+	alias jn='cd ~/Dropbox/jupyter_notebooks;jupyter notebook'
 	;;
 esac
