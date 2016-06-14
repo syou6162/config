5efc574ad628b2993b0e7a4605613d82dea0e8ff
Yasuhisa Yoshida <syou6162@gmail.com>
2016-06-15 00:27:41 +0900

    インデント直す

--- custom/osx.zsh
+++ custom/osx.zsh
@@ -1,7 +1,7 @@
 case "${OSTYPE}" in
-darwin*)
-	alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
-	export EDITOR=emacsclient
-	alias jn='cd ~/Dropbox/jupyter_notebooks;jupyter notebook'
-	;;
+  darwin*)
+    alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
+    export EDITOR=emacsclient
+    alias jn='cd ~/Dropbox/jupyter_notebooks;jupyter notebook'
+    ;;
 esac
