6fe2d8472ed438610d3154b0beb3a298c1d8f982
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-20 23:31:05 +0900

    zshenvに環境変数を逃がす

--- custom/my-orig-zsh.zsh
+++ custom/my-orig-zsh.zsh
@@ -1,8 +1,6 @@
 fpath=(~/.zsh/functions/Completion/ $fpath)
 autoload -U compinit
 compinit
-export LC_ALL=ja_JP.UTF-8
-export LANG=ja_JP.UTF-8
 
 PROMPT="%/%% "
 PROMPT2="%_%% "
@@ -85,8 +83,6 @@ case "${TERM}" in
   ;;
 esac
 
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-
 zstyle ':completion:*:default' menu select=1
 alias ls='ls -G'
 
@@ -111,21 +107,6 @@ do
 done
 
 eval "$(direnv hook zsh)"
-
-export PATH="$HOME/.plenv/bin:$PATH"
-if which plenv > /dev/null; then eval "$(plenv init -)"; fi
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-export PATH="$HOME/.ndenv/bin:$PATH"
-if which ndenv > /dev/null; then eval "$(ndenv init -)"; fi
-
-export PATH="$HOME/.pyenv/bin:$PATH"
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$PATH
 
 setopt no_global_rcs
 
