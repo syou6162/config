09d65c55d6525590433a7bba2f77c8803b67f482
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-26 13:36:26 +0900

    auto-completeでC-nとかで移動できるように

---
+++ .emacs.d/inits/40-auto-complete.el
@@ -0,0 +1,5 @@
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+(define-key ac-completing-map (kbd "C-n") 'ac-next)
+(define-key ac-completing-map (kbd "C-p") 'ac-previous)
