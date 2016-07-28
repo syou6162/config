19b60a97a13035b6ddf603fa7a5b2605ba7d8260
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-28 21:33:20 +0900

    indentまわり修正

--- .emacs.d/inits/40-web-mode.el
+++ .emacs.d/inits/40-web-mode.el
@@ -5,10 +5,11 @@
 ;; インデント関係
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-html-offset   4)
-  (setq web-mode-css-offset    4)
+  (setq web-mode-html-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-offset 4)
   (setq web-mode-script-offset 4)
-  (setq indent-tabs-mode t)
-  (setq tab-width 4))
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
 
 (add-hook 'web-mode-hook 'web-mode-hook)
