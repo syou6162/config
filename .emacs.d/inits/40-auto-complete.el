bda0c23628c1aeb9e318d5f64d71a0e4cdc1d3a3
Yasuhisa Yoshida <syou6162@gmail.com>
2016-10-05 16:47:25 +0900

    auto-completeの色を変える

--- .emacs.d/inits/40-auto-complete.el
+++ .emacs.d/inits/40-auto-complete.el
@@ -3,3 +3,11 @@
 (global-auto-complete-mode t)
 (define-key ac-completing-map (kbd "C-n") 'ac-next)
 (define-key ac-completing-map (kbd "C-p") 'ac-previous)
+
+(set-face-background 'ac-completion-face "white")
+(set-face-foreground 'ac-candidate-face "black")
+(set-face-background 'ac-selection-face "black")
+(set-face-foreground 'ac-selection-face "red")
+(set-face-foreground 'popup-summary-face "black") ;; 候補のサマリー部分
+(set-face-background 'popup-tip-face "cyan") ;; ドキュメント部分
+(set-face-foreground 'popup-tip-face "white")
