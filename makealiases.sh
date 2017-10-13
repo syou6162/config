2aa9803fb0abaaf1312c0390735af1e092aec7df
Yasuhisa Yoshida <syou6162@gmail.com>
2017-10-13 13:17:08 +0900

    シンボリックリンク張る

--- makealiases.sh
+++ makealiases.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $HOME以下に設定ファイル群へのシンボリックリンクを貼る
-for filename in emacs.d zshrc zshenv Rprofile gitconfig gitignore tmux.conf
+for filename in emacs.d zshrc zshenv Rprofile gitconfig gitignore tmux.conf vimrc tigrc
 do
     if [ ! -e $HOME/.$filename ]; then
 	ln -s `pwd`/.$filename $HOME/.$filename
