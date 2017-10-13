7181d51bfaf175be792e19c37bf4752a255f0352
Yasuhisa Yoshida <syou6162@gmail.com>
2017-10-14 06:17:24 +0900

    delete osx.zsh

--- custom/osx.zsh
+++
@@ -1,7 +0,0 @@
-case "${OSTYPE}" in
-  darwin*)
-    alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
-    export EDITOR=vim
-    alias jn='cd ~/Dropbox/jupyter_notebooks;jupyter notebook'
-    ;;
-esac
