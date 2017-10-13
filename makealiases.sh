74ab6ffd9f0822a7f4cb2360bf11d1eb4c589f7d
Yasuhisa Yoshida <syou6162@gmail.com>
2017-10-14 06:14:00 +0900

    oh-my-zshをやめる

--- makealiases.sh
+++ makealiases.sh
@@ -7,11 +7,3 @@ do
 	ln -s `pwd`/.$filename $HOME/.$filename
     fi
 done
-
-# oh-my-zsh用にシンボリックリンクを貼る
-for file in custom/*
-do
-    if [ ! -e $HOME/.oh-my-zsh/$file ]; then
-	ln -s `pwd`/$file $HOME/.oh-my-zsh/$file
-    fi
-done
