ad76661f41ca97cba0032d85dfad676cb3ad1b0f
Yasuhisa Yoshida <syou6162@gmail.com>
2017-10-14 18:55:00 +0900

    peco_zを使えるようにする

--- config.fish
+++ config.fish
@@ -4,6 +4,7 @@ set -x TERM xterm-256color
 function fish_user_key_bindings
   bind \c] peco_select_ghq_repository
   bind \cr peco_select_history
+  bind \x1b peco_z # Ctrl-[にバインドする
 end
 
 set -g theme_display_ruby no
