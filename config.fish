ba6a367d6206d63e4244c22805d844b6eb48e1f7
Yasuhisa Yoshida <syou6162@gmail.com>
2017-10-16 12:11:38 +0900

    fishの履歴を同期する

--- config.fish
+++ config.fish
@@ -8,9 +8,19 @@ set -x PATH $GOPATH/bin $PATH
 set -x EDITOR vim
 set -x TERM xterm-256color
 
+function history-merge --on-event fish_preexec
+  history --save
+  history --merge
+end
+
+function peco_sync_select_history
+  history-merge
+  peco_select_history $argv
+end
+
 function fish_user_key_bindings
   bind \c] peco_select_ghq_repository
-  bind \cr peco_select_history
+  bind \cr peco_sync_select_history
   bind \x1b peco_z # Ctrl-[にバインドする
 end
 
