e1436b9a95b7f44fde908cdff9dc1e35fcee0e69
Yasuhisa Yoshida <syou6162@gmail.com>
2019-11-29 18:32:06 +0900

    fisherインストール

--- config.fish
+++ config.fish
@@ -32,3 +32,9 @@ set -g theme_display_ruby no
 set -g theme_display_date no
 
 eval (direnv hook fish)
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
