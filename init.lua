83400613860d78b8136871526f7eba05d7b7eef4
Yasuhisa Yoshida <syou6162@gmail.com>
2019-11-29 22:52:49 +0900

    hammerspoon

---
+++ init.lua
@@ -0,0 +1,14 @@
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+remapKey({'ctrl'}, 'j', keyCode('kana'))
