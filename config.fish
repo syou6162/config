5f2b6d8d0d95f1830ccde09d52f7ac22e8441cd3
Yasuhisa Yoshida <syou6162@gmail.com>
2022-09-16 07:00:36 +0900

    tmux用に環境変数でfishのパスを読むように設定

--- config.fish
+++ config.fish
@@ -19,6 +19,8 @@ case x86_64
   source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
 end
 
+set -x TMUX_SHELL (which fish)
+
 status is-login; and pyenv init --path | source
 
 status --is-interactive; and . (rbenv init -|psub)
