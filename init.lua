c89aefd41496afa3cfaf9fa9eda53af37bf1b1fd
Yasuhisa Yoshida <syou6162@gmail.com>
2024-02-12 07:46:33 +0900

    ホットキーの割り当て

--- init.lua
+++ init.lua
@@ -12,3 +12,26 @@ local function remapKey(modifiers, key, keyCode)
 end
 
 remapKey({'ctrl'}, 'j', keyCode('kana'))
+
+-- ホットキーの割り当て
+-- アプリ名だとVSCodeで動かない場合があるため、bundleIDを使う
+-- ref: https://github.com/Hammerspoon/hammerspoon/issues/2075
+-- bundleIDは以下のコマンドで取得できる
+-- lsappinfo info -only bundleid Finder
+
+function toggleApp(key, modifiers, bundleID)
+  hs.hotkey.bind(key, modifiers, function()
+    local app = hs.application.get(bundleID)
+    if app == nil then
+      hs.application.launchOrFocusByBundleID(bundleID)
+    elseif app:isFrontmost() then
+      app:hide()
+    else
+      hs.application.launchOrFocusByBundleID(bundleID)
+    end
+  end)
+end
+
+toggleApp({"ctrl", "shift"}, "s", "com.tinyspeck.slackmacgap") -- Slack
+toggleApp({"ctrl", "shift"}, "c", "com.google.Chrome")
+toggleApp({"ctrl"}, "i", "com.microsoft.VSCode")
