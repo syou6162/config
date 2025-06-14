9f512fbe9869f17bda81e9aff7548c57a2c9e44f
Yasuhisa Yoshida <syou6162@gmail.com>
2025-06-14 10:16:05 +0900

    fzfのdefaultオプションを設定

--- config.fish
+++ config.fish
@@ -75,3 +75,5 @@ end
 function gws
   cd (git worktree list | peco | awk '{print $1}')
 end
+
+set -gx FZF_DEFAULT_OPTS '--exact --bind "enter:become(vim {})" --preview "bat --color=always --style=header,grid --line-range :300 {}" --preview-window "up:80%:border"'
